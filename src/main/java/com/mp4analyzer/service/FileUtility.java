d4e12156f55d3d7f296bf4a8c82d057adee93e23
Lance Fallon <fallon.lance@gmail.com>
2019-04-09 16:31:17 -0400

    refactor zip methods into fileUtility service

--- src/main/java/com/mp4analyzer/service/FileUtility.java
+++ src/main/java/com/mp4analyzer/service/FileUtility.java
@@ -4,10 +4,18 @@ import com.mp4analyzer.model.FileSearchType;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Service
 public class FileUtility {
+
+    private static final Logger log = Logger.getLogger(FileUtility.class.getName());
 
     public void deleteFiles(List<File> files) {
         for(File file : files){
@@ -29,6 +37,17 @@ public class FileUtility {
     }
 
     public String getDisplayName(File file) {
-        return file.getName().split("0")[0];
+        return file.getName().split("\\.")[0];
+    }
+
+    public void addToZipFile(String zipAbsFilePath, List<File> filesToZip) {
+        try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipAbsFilePath))) {
+            for (File fileToZip : filesToZip) {
+                zipOut.putNextEntry(new ZipEntry(fileToZip.getName()));
+                Files.copy(fileToZip.toPath(), zipOut);
+            }
+        } catch(IOException e){
+            log.warning(e.toString());
+        }
     }
 }
--- src/main/java/com/mp4analyzer/service/ScreenGrabService.java
+++ src/main/java/com/mp4analyzer/service/ScreenGrabService.java
@@ -3,6 +3,7 @@ package com.mp4analyzer.service;
 import com.mp4analyzer.model.FileSearchType;
 import com.mp4analyzer.model.ScreenGrabMeta;
 import com.mp4analyzer.repository.ScreenGrabRepository;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
@@ -15,11 +16,13 @@ import org.springframework.stereotype.Service;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -54,7 +57,8 @@ public class ScreenGrabService {
         Random random = new Random();
         long millis = System.currentTimeMillis();
         String folderLocation = baseDirectory + "out\\" + filename + "_" + millis + "\\";
-        new File(folderLocation).mkdir();
+        File folder = new File(folderLocation);
+        folder.mkdir();
 
         List<File> photoList = new ArrayList<>();
         try {
@@ -79,46 +83,15 @@ public class ScreenGrabService {
                 log.info(String.format("Screen grabbed at %d seconds for %s", seconds, filename));
             }
 
-            //update the db record with new screenCapture path & timestamp
-            //screenGrabRepository.updateScreenGrab(new ScreenGrabMeta(screenCapture.getAbsolutePath(), frameLocation, g.getFrameRate()));
-
             //zip files
-            addToZipFile(baseDirectory + "out\\" + filename + "_" + millis + ".zip", photoList);
+            fileUtility.addToZipFile(baseDirectory + "out\\" + filename + "_" + millis + ".zip", photoList);
 
             //delete original folder
-            new File(folderLocation).delete();
+            FileUtils.deleteDirectory(folder);
 
             g.stop();
 
-            //fileUtility.deleteFiles(existingImages); //delete the old photos
-
         } catch (FrameGrabber.Exception | IOException e) {
-            log.warning(e.toString());
-        }
-    }
-
-    private void addToZipFile(String filename, List<File> filesToZip) throws FileNotFoundException, IOException {
-
-        try(ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(filename))) {
-
-            for (File file : filesToZip) {
-
-                try(FileInputStream fis = new FileInputStream(file)) {
-                    ZipEntry zipEntry = new ZipEntry(file.getName());
-                    zos.putNextEntry(zipEntry);
-
-                    byte[] bytes = new byte[1024];
-                    int length;
-                    while ((length = fis.read(bytes)) >= 0) {
-                        zos.write(bytes, 0, length);
-                    }
-
-                    zos.closeEntry();
-                } catch(IOException e){
-                    log.warning(e.toString());
-                }
-            }
-        } catch(IOException e){
             log.warning(e.toString());
         }
     }
